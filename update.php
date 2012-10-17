bac9af3117baea14cf0e0887ec3eea801786cd57
Jeroen Van den Bossche <vanbosse.jeroen@gmail.com>
2012-10-17 16:40:01 +0200

    Update: send out an update every 3 seconds.

--- update.php
+++ update.php
@@ -8,18 +8,22 @@ $channel = $connection->channel();
 
 // Create a fanout exchange.
 // A fanout exchange broadcasts to all known queues.
-$channel->exchange_declare('updates', 'fanout');
+$channel->exchange_declare('updates', 'fanout', false, false, false);
 
 // Create and publish the message to the exchange.
-$data = array(
-    'type' => 'update',
-    'data' => array(
-        'minutes' => rand(0, 60),
-        'seconds' => rand(0, 60)
-    )
-);
-$message = new AMQPMessage(json_encode($data));
-$channel->basic_publish($message, 'updates');
+while (true)
+{
+    $data = array(
+        'type' => 'update',
+        'data' => array(
+            'minutes' => rand(0, 60),
+            'seconds' => rand(0, 60)
+        )
+    );
+    $message = new AMQPMessage(json_encode($data));
+    $channel->basic_publish($message, 'updates');
+    sleep(3);
+}
 
 // Close connection.
 $channel->close();
